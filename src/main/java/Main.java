ab94ab277bf7f9855501e961788b535487c9df3f
yinm <yinm.cminor9@gmail.com>
2018-01-02 22:05:33 +0900

    Refactor by extracting method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,22 @@ public class Main {
 	public static void main(String[] args) {
 		for (int i = 0; i < 100; i++) {
 			String out = "";
-			if (i % 3 == 0) {
-				out += "fizz";
-			}
-			if (i % 5 == 0) {
-				out += "buzz";
-			}
-			if (i % 3 != 0 && i % 5 != 0) {
-				out += i;
-			}
+			out = fizzbuzz(i, out);
 			System.out.println(out);
 		}
+	}
+
+	static String fizzbuzz(int i, String out) {
+		if (i % 3 == 0) {
+			out += "fizz";
+		}
+		if (i % 5 == 0) {
+			out += "buzz";
+		}
+		if (i % 3 != 0 && i % 5 != 0) {
+			out += i;
+		}
+
+		return out;
 	}
 }
