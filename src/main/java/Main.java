2803178066d681fd37a4f3af7fabb127be3b2253
yinm <yinm.cminor9@gmail.com>
2018-01-02 22:00:49 +0900

    Add fizzbuzz

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			String out = "";
+			if (i % 3 == 0) {
+				out += "fizz";
+			}
+			if (i % 5 == 0) {
+				out += "buzz";
+			}
+			if (i % 3 != 0 && i % 5 != 0) {
+				out += i;
+			}
+			System.out.println(out);
+		}
+	}
+}
